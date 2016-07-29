a5a7ee9a1ad5599a49c6c0a46f6dee15cdbd2d12
ComanGames <alfaa.gen@gmail.com>
2016-07-29 18:35:25 +0300

    Add project files.

---
+++ HalpBarter/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace HalpBarter
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
