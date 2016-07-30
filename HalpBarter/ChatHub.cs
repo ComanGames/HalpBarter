1a760057a7789fe1e678042bd75e0ddc99d09023
ComanGames <alfaa.gen@gmail.com>
2016-07-30 19:57:33 +0300

    Message improvements

--- HalpBarter/ChatHub.cs
+++ HalpBarter/ChatHub.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
 
 namespace HalpBarter
 {
     public class ChatHub : Hub
     {
-        public void Send(string name, string text)
+        public void Send(string userName, string text)
         {
-            Clients.All.send(name,text);
+            Clients.All.send(userName,text);
         }
     }
 }
